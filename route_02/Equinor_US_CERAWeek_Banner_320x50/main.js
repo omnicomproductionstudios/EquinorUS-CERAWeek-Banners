0a57b2e213faedf79cc9f9ffd8c174c32c45d3f8
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2026-03-24 22:30:34 +0530

    dfb

--- route_02/Equinor_US_CERAWeek_Banner_320x50/main.js
+++ route_02/Equinor_US_CERAWeek_Banner_320x50/main.js
@@ -11,7 +11,6 @@ function init() {
   // Set Global Timeline
   tl = gsap.timeline({ onComplete: endTime });
   animate();
-  setRollover();
 }
 
 function animate() {
@@ -19,35 +18,15 @@ function animate() {
   tl.set([".copy1Container, .copy2Container, #cta"], { x: 40, force3D: true });
   
   tl.to("#logo", 0.5, {autoAlpha:1}, '0');
-  // tl.to(".copy1Container", 1.5, { x:0, ease: Power1.easeOut}, '+=0.5');
-  // tl.staggerTo(["#copy1a", "#copy1b"], 1, {autoAlpha:1,  clipPath: "inset(0% 0% 0% 0%)", ease: Power1.easeOut}, 0.5, '<');
 
   tl.to(".copy1Container", 1.5, { x: 0, ease: Power1.easeOut }, "+=0.5");
 
-  tl.staggerTo(["#copy1a", "#copy1b"], 1, {autoAlpha:1,  clipPath: "inset(0% 0% 0% 0%)", ease: Power1.easeOut}, 0.6, '<');
+  tl.staggerTo(["#copy1a", "#copy1b"], 1, {autoAlpha:1,  clipPath: "inset(0% 0% 0% 0%)", ease: Power1.easeOut}, 0.7, '<');
 
   tl.to(".copy1Container", 0.5, {autoAlpha:0, ease: Power1.easeOut}, '+=2.5');
 
   tl.to(".copy2Container", 1.5, { x:0, ease: Power1.easeOut}, '>');
   tl.staggerTo(["#copy2a", "#copy2b"], 1, {autoAlpha:1,  clipPath: "inset(0% 0% 0% 0%)", ease: Power1.easeOut}, 0.5, '<');
-}
-
-
-function setRollover() {
-  document
-    .getElementById("default_exit")
-    .addEventListener("mouseover", default_over, false);
-  document
-    .getElementById("default_exit")
-    .addEventListener("mouseout", default_out, false);
-}
-
-function default_over(event) {
-  gsap.to(".ctaArrow", 0.2, {x: 5, ease: "none"});
-}
-
-function default_out(event) {
-  gsap.to(".ctaArrow", 0.2, {x: 0, ease: "none"  });
 }
 
 
